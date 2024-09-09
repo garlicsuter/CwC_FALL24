631755cec23c27ac31fca35b427476c54c9c5da5
Mark Suter <msuter@elida.k12.oh.us>
2024-09-09 10:51:25 -0400

    Camera follows player
    a

--- Unit1_CwC_FALL24/Assets/Scripts/FollowPlayer.cs
+++ Unit1_CwC_FALL24/Assets/Scripts/FollowPlayer.cs
@@ -2,9 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
+public class FollowPlayer : MonoBehaviour
 {
-    public float speed = 20.0f;
+    public GameObject player;
 
     // Start is called before the first frame update
     void Start()
@@ -15,7 +15,6 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //move the tank forward
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        transform.position = player.transform.position;
     }
 }
